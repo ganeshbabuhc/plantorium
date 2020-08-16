cc2b1cbf1ddd52aed0dabdd1f24e47764e35e8eb
ganeshbabuhc <ganesh99454@gmail.com>
2020-08-16 10:58:46 +0530

    2 files modified

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react";
+import fetch from 'node-fetch';
 
 import API from "../service/API";
+import Constants from "../utils/Constants";
 const Search = () => {
 
-const getPlants = () => {
-   const data = API.getURL('plants');
-   console.log("asasa", data);
+const getPlants = async () => {
+  console.log(fetch);
+  //  const data = API.getURL('plants');
+  //   const apiUrl = `${Constants.API_HOST}${'plants'}?token=${Constants.API_TOKEN}`;
+  //    const response = await fetch(apiUrl, { method: 'GET'});
+  //    console.log(response);
+  //   const data = await response.json();
+  //  console.log("asasa", data);
   //  conso
     
 }
--- service/API.ts
+++ service/API.ts
@@ -1,12 +1,12 @@
 import Constants from '../utils/Constants';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 class API {
   public static getURL = (url:string): string => {
     return `${Constants.API_HOST}${url}?token=${Constants.API_TOKEN}`;
   };
-  public static getData = async (url:string): Promise<any> => {
+  public getData = async (url:string) => {
+    console.log('get api data');
     const apiUrl = `${Constants.API_HOST}${url}?token=${Constants.API_TOKEN}`;
-    
      const response = await fetch(apiUrl, { method: 'GET'});
      console.log(response);
     return await response.json();
