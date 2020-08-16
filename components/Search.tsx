f6de626bb60edcdc2bdc3f2ebb0538f9592e89cc
ganeshbabuhc <ganesh99454@gmail.com>
2020-08-16 11:33:06 +0530

    2 files modified

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import fetch from 'node-fetch';
 
 import API from "../service/API";
 import Constants from "../utils/Constants";
@@ -8,12 +7,28 @@ const Search = () => {
 const getPlants = async () => {
   console.log(fetch);
   //  const data = API.getURL('plants');
-  //   const apiUrl = `${Constants.API_HOST}${'plants'}?token=${Constants.API_TOKEN}`;
-  //    const response = await fetch(apiUrl, { method: 'GET'});
-  //    console.log(response);
+    const apiUrl = `${Constants.API_HOST}${'plants'}?token=${Constants.API_TOKEN}`;
+  //    const response = await fetch(apiUrl, { method: 'GET'}).then(res => response.json()).then(res => {
+  //       console.log(res);
+  //    });
+  //   //  console.log(response);
   //   const data = await response.json();
   //  console.log("asasa", data);
   //  conso
+
+
+  const response = await fetch(apiUrl, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors,
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  response.json().then(data => {
+    console.log(data);
+  });
+  // console.log(data)
     
 }
   useEffect(() => {
--- service/API.ts
+++ service/API.ts
@@ -8,7 +8,6 @@ class API {
     console.log('get api data');
     const apiUrl = `${Constants.API_HOST}${url}?token=${Constants.API_TOKEN}`;
      const response = await fetch(apiUrl, { method: 'GET'});
-     console.log(response);
     return await response.json();
   };
 }
